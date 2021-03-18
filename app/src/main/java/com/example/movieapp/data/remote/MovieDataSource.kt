0a5a69221262cbd01331f99749a12ad54034fd31
Guillermo Echague <guillermo.echaguea@gmail.com>
2021-03-18 03:33:27 -0300

    Concluyendo el DataSource

--- app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
@@ -1,16 +1,14 @@
 package com.example.movieapp.data.remote
 
+import com.example.movieapp.application.AppConstants
 import com.example.movieapp.data.model.MovieList
+import com.example.movieapp.repository.WebService
 
-class MovieDataSource {
-    fun getUpcomingMovies(): MovieList{
-        return MovieList()
-    }
-    fun getTopRatedMovies(): MovieList {
-        return MovieList()
-    }
-    fun getPopularMovies(): MovieList {
-        return MovieList()
-    }
+class MovieDataSource(private val webService: WebService) {
+    suspend fun getUpcomingMovies(): MovieList = webService.getUpcomingMovies(AppConstants.API_KEY)
+
+    suspend fun getTopRatedMovies(): MovieList = webService.getTopRatedMovies(AppConstants.API_KEY)
+
+    suspend fun getPopularMovies(): MovieList = webService.getPopularMovies(AppConstants.API_KEY)
 
 }
