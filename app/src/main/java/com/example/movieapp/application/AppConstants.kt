83eea414d457ce37468d4e35c66dec5bd7b1b478
Guillermo Echague <guillermo.echaguea@gmail.com>
2021-03-18 03:25:34 -0300

    Capa de Repositorio

---
+++ app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.application
+
+object AppConstants {
+
+    //API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "d835ab864927b347c03da32cf48e109f"
+}
